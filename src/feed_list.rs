ddaa305855a8e8488c3d9f5bfc1beabc158ce637
goverclock <goverclock@outlook.com>
2024-03-06 20:44:56 +0800

    fetch and parse feeds

--- src/feed_list.rs
+++ src/feed_list.rs
@@ -1,19 +1,38 @@
 use eframe::egui::{Button, Color32, ScrollArea, Sense, Stroke, Ui, Vec2};
+use rss::Channel;
 pub struct FeedList {
     selected_feed: Option<usize>,
+    // feeds: Vec<>
+    feed_items: Vec<rss::Item>,
 }
 
 impl FeedList {
     pub fn new() -> Self {
-        Self { selected_feed: None }
+        // initial feeds
+        // let f = std::fs::File::open("twit.xml").unwrap();
+        // let chan = Channel::read_from(BufReader::new(f)).unwrap();
+        // println!("{:?}", chan.items().len());
+
+        let content = reqwest::blocking::get("https://feeds.twit.tv/twit.xml")
+            .unwrap()
+            .bytes()
+            .unwrap();
+        let chan = Channel::read_from(&content[..]).unwrap();
+        let feed_items = chan.into_items();
+
+        Self {
+            selected_feed: None,
+            feed_items,
+        }
     }
 
     pub fn ui(&mut self, ui: &mut Ui) {
         ui.heading("there should be rss feeds");
         let size = ui.min_size();
         ScrollArea::vertical().show(ui, |ui| {
-            for i in 0..10 {
-                let mut btn = Button::new("Fuck")
+            let fi = &self.feed_items;
+            for i in 0..fi.len() {
+                let mut btn = Button::new(fi[i].title().unwrap_or("titleless feed"))
                     .sense(Sense::click())
                     .stroke(Stroke::new(0.0, Color32::TRANSPARENT))
                     .rounding(3.0)
